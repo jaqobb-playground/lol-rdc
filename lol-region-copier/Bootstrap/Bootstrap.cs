f31399c37a401dfba3f1434245bfe781c0268d2c
Jakub Zagórski <jaqobbdev@gmail.com>
2019-01-31 17:14:00 +0100

    Testing IDE stuff.

---
+++ lol-region-copier/Bootstrap/Bootstrap.cs
@@ -0,0 +1,13 @@
+﻿using lol_region_copier.Core;
+
+namespace lol_region_copier.Bootstrap
+{
+	public partial class Bootstrap
+	{
+		private static void Main(string[] arguments)
+		{
+			LoLRegionCopier regionCopier = new LoLRegionCopier();
+			regionCopier.Start();
+		}
+	}
+}
---
+++ lol-region-copier/Core/LoLRegionCopier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lol_region_copier.Core
+{
+	public class LoLRegionCopier
+	{
+		public void Start()
+		{
+			Console.WriteLine("Preparing region copier...");
+			Console.WriteLine("Finding settings file...");
+			Console.ReadKey();
+		}
+	}
+}
